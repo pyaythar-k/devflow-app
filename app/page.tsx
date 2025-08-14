86e05de4f20dea1140cd3586cc8819209a5ed358
pyaythar-k <pyaythar.k@gmail.com>
2025-08-14 15:27:28 +0900

    Assets and metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 const page = () => {
-  return <div>Welcome to Next.js</div>;
+  return (
+    <>
+      <h1>Hello World</h1>
+      <h1 className="font-space-grotesk">Hello World</h1>
+    </>
+  );
 };
 
 export default page;
